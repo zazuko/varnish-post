87ca5ac7523323404576afa6b67b88a1dd9bd4d8
Ludovic Muller <ludovicmuller1@gmail.com>
2022-02-03 11:28:07 +0100

    test/app: upgrade some versions

--- test/app/.eslintrc.js
+++ test/app/.eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
